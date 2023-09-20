51708af2d3a38ddd5858bd4e01f30e70befb745c
ENG_Osama Elsayes <109886909+osama658@users.noreply.github.com>
2023-09-20 13:19:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-import os 
+import os
 import sqlite3
 import helpers
 
@@ -7,4 +6,26 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
+
 from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# start emplaminting the main page here!. TODO
