cf7fabce321758e0282ebf23398719d80a160c96
ENG_Osama Elsayes <109886909+osama658@users.noreply.github.com>
2023-09-20 11:45:15 +0300

    inserting the librares and configer the -flask- application.

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-# nothing yet, OSAMA
+
+import os 
+import sqlite3
+import helpers
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required, lookup, usd
